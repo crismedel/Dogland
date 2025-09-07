4858b4cb7a6d92f6df3cb70ec8c7dacdf5cbaf7b
BayronFlores <floresbayron133@gmail.com>
2025-09-06 21:58:04 -0400

    feat(organizations): implementar endpoints REST para organizaciones

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const app = express();
 
+import alertsRouter from './routes/alerts.js';
+import organizationsRouter from './routes/organizations.js';
+
 // conffiguraciones
 app.set('port', 3000);
 
@@ -8,6 +11,9 @@ app.set('port', 3000);
 app.get('/', (req, res) => {
   res.send('Servidor funcionando correctamente');
 });
+
+app.use('/api', alertsRouter);
+app.use('/api', organizationsRouter);
 
 app.listen(app.get('port'), () => {
   console.log(`Servidor correindo en http://localhost:${app.get('port')}`);
