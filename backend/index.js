e9e42260cc70be151892633c9e0f36ba9f2b2e1c
Bad4steroid <sesparza2020@alu.uct.cl>
2025-09-09 03:33:53 -0300

    feat: crear endpoint PUT /password-reset-confirm para actualizar contraseña

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import alertsRouter from './routes/alerts.js';
 import organizationsRouter from './routes/organizations.js';
 import usersRouter from './routes/users.js';
 import passwordResetRequestRouter from './routes/password-reset-request.js';
+import passwordResetConfirmRouter from './routes/password-reset-confirm.js';
 
 // conffiguraciones
 app.set('port', 3000);
@@ -19,6 +20,7 @@ app.use('/api', alertsRouter);
 app.use('/api', organizationsRouter);
 app.use('/api', usersRouter);
 app.use("/api", passwordResetRequestRouter);
+app.use("/api", passwordResetConfirmRouter);
 
 app.listen(app.get('port'), () => {
   console.log(`Servidor correindo en http://localhost:${app.get('port')}`);
