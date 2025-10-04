b1572951eee638153c62b66b21ad953b41014c7a
Cristóbal Ignacio Medel Campos <crismedel17@gmail.com>
2025-10-04 18:39:56 -0300

    Eliminación de archivos de backend en la rama dev-front

--- backend/middlewares/blacklist.js
+++
@@ -1,9 +0,0 @@
-const blacklistedTokens = new Set();
-
-export const blacklistToken = (token) => {
-  blacklistedTokens.add(token);
-};
-
-export const isTokenBlacklisted = (token) => {
-  return blacklistedTokens.has(token);
-};
