e24f9637772f5136e57361f1e396024a1eced8d7
Cristóbal Medel <crismedel17@gmail.com>
2025-11-07 22:05:37 -0300

    fix: corrección de nombres de rutas en backend
    - se ajustaron los nombres de rutas para razas.js y especiesController.js en el backend

--- backend/routes/razas.js
+++ backend/routes/razas.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getRazas } from '../controllers/razasController.js';
+import { getRaces } from '../controllers/razasController.js';
 // import { authenticateToken } from '../middlewares/auth.js'; // Descomenta si este catálogo es privado
 
 const router = express.Router();
 
 // Define la ruta GET /api/razas
-router.get('/razas', getRazas);
+router.get('/razas', getRaces);
 
 export default router;
